df4ecdd3d191d60c47d80928c8b2f5a3ad93df34
Mans Matulewicz <mans.matulewicz@gmail.com>
2015-11-15 13:11:51 +0100

    added hiera

--- puppet-local/manifests/puppetmaster.pp
+++ puppet-local/manifests/puppetmaster.pp
@@ -5,9 +5,15 @@ class { 'r10k':
       'remote'  => 'https://github.com/MansM/vagrant-skeleton-r10k.git',
       'basedir' => "${::settings::confdir}/environments",
       'prefix'  => false,
+    }, 
+    'hiera' => {
+      'remote'  => 'https://github.com/MansM/vagrant-skeleton-hiera.git',
+      'basedir' => "${::settings::confdir}/hiera",
+      'prefix'  => true,
     }
   },
   #purgedirs         => ["${::settings::confdir}/environments"],
   manage_modulepath => true,
   modulepath        => "${::settings::confdir}/environments/\$environment/modules:/opt/puppet/share/puppet/modules",
 }
+
